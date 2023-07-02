c5ace1f0d7dfaa486d8311de8047a86f04fda4f2
Alois Zimmermann <alois.zimmermann2003@gmail.com>
2023-07-02 18:19:40 +0200

    Init project

---
+++ alpha/__main__.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+'''This is the main file.
+It starts alpha.
+'''
+
+# Import modules and files
+import os
+from dotenv import load_dotenv
+import requests
+import io
+import openai
+import boto3
+import speech_recognition as sr
+from playsound import playsound
+from PIL import Image
+
+import utils
+
+
+def load_api_keys():
+    load_dotenv()
+
+def process_command(query):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    model_name = 'gpt-3.5-turbo'
+    response = openai.ChatCompletion.create(
+        model=model_name,
+        messages=[
+            {"role": "system", "content": "Du bist meine Künstliche Intelligenz namens Alpha. Ich habe dich erschaffen, um mir im Leben zu helfen."},
+            {"role": "user", "content": query}
+        ],
+        temperature=0.5
+    )
+    reply = response.choices[0].message.content
+    return reply
+
+def recognize_speech():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        utils.logger.info("Sprechen Sie einen Befehl")
+        audio = r.listen(source)
+    try:
+        command = r.recognize_google(audio, language="de-DE")
+        utils.logger.info("Erkannter Befehl: " + command)
+        return command
+
+    except sr.UnknownValueError:
+        utils.logger.info("Befehl konnte nicht erkannt werden")
+        return ""
+def speak(text):
+    polly = boto3.client('polly',
+                         region_name='eu-central-1',
+                         aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                         aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))
+    response = polly.synthesize_speech(Text=text,
+                                       OutputFormat='mp3',
+                                       VoiceId='Daniel',
+                                       Engine='neural',
+                                       LanguageCode='de-DE')
+    
+    audio_stream = response['AudioStream'].read()
+    with open('output.mp3', 'wb') as file:
+        file.write(audio_stream)
+    playsound('output.mp3')
+
+def generate_image(picture_query):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    response = openai.Image.create(
+        prompt=picture_query,
+        n=1,
+        size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+    return image_url
+def display_image(image_url):
+    response = requests.get(image_url)
+    image = Image.open(io.BytesIO(response.content))
+    image.show()
+
+if __name__ == "__main__":
+    load_api_keys()
+    while True:
+        command = recognize_speech()
+        if command:
+            if "bild" in command.lower():
+                image_url = generate_image(command)
+                display_image(image_url)
+                continue
+            response = process_command(command)
+            speak(response)
---
+++ utils/logger.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''This is the logger file.
+It contains the logger configuration.
+'''
+
+# Import modules and files
+import logging
+import colorlog
+
+
+# Configure logging variables
+LOG_LEVEL = logging.DEBUG
+LOGFORMAT = '%(log_color)s%(levelname)-10s%(reset)s[%(asctime)s] %(name)s: %(message)s'
+DATEFORMAT = '%d/%b/%Y %H:%M:%S'
+
+# Logger erstellen
+logger = colorlog.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# Set formatter
+formatter = colorlog.ColoredFormatter(LOGFORMAT, datefmt=DATEFORMAT,
+    log_colors={
+        'DEBUG': 'white',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red',
+            'SUCCESS': 'green'
+    })
+
+handler = colorlog.StreamHandler()
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
